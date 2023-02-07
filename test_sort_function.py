6e1ae8ea4ee4fa7579548e334e0f10f0f70d6d73
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-02-07 14:27:40 -0800

    Modify unit tests

--- test_sort_function.py
+++ test_sort_function.py
@@ -6,8 +6,10 @@ from sort import is_sorted
 class TestSorted(TestCase):
     def test_is_sorted(self):
         self.assertEqual(True, is_sorted([1, 2, 3]))
+
     def test_is_sorted_decreasing(self):
         self.assertEqual(False, is_sorted([12, 6, 2]))
+
     def test_is_sorted_zeroes(self):
         self.assertEqual(True, is_sorted([0, 0, 0]))
 
